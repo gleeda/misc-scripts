3c32909a3fec1c5d66345ef3ea0dcc3df0a52dc7
gleeda <jamie@memoryanalysis.net>
2016-02-01 16:40:57 -0500

    fixed to allow parameters and user/passwd on the fly

--- fresponse/fresponse.bash
+++ fresponse/fresponse.bash
@@ -1,7 +1,7 @@
 # Credentials for the remote machine
 # so you can install fresponse remotely
-REMOTEUSER=
-REMOTEPASS=
+REMOTEUSER=NONE
+REMOTEPASS=NONE
 
 # IP addresses of remote machines:
 # example IPs below, replace with your own:
@@ -11,15 +11,15 @@ IPs=(192.168.1.143 192.168.1.134)
 # You can change the name of the executable
 #  but the INI file should be renamed accordingly too
 # These files should be in the same directory as this script
-EXE=f-response-ent.exe   
-INI=f-response-ent.exe.ini 
+EXE=f-response-ent.exe
+INI=f-response-ent.exe.ini
 # F-Reponse port:
 PORT=3260
 # F-Response username and password:
-FRUSER=
-FRPASS=
+FRUSER=NONE
+FRPASS=NONE
 # Name of service on remote machine:
-SERV=fresponse  
+SERV=fresponse
 # Location of agent and INI file on remote machine:
 WIN=WINDOWS
 # All disks, memory, volumes that are mounted:
@@ -33,19 +33,64 @@ RESOURCES=()
 ##  fresponse.bash: F-Response Enterprise Helper Script
 ##
 ##   Allows one to install and start the F-Response
-##      service from a Linux machine 
+##      service from a Linux machine
 ##   Also automates the discovery and logging into
 ##      F-Response nodes
 ##   This is a quick and dirty script and should
 ##      be expanded as needed...
+##   Very minimal error checking
 ##
 #######################################################
 
+bad=-65
+usage="Usage: $0 \n\t-I [IP addresses, comma delimited]\n\t-P [Port for F-Response. Default: $PORT]"
+usage="$usage\n\t-S [Service name for F-Reponse. Default: $SERV]\n\t-L [Location to install agent. Default: C:\\$WIN]"
+usage="$usage\n\t-E [Path to F-Response executable. Default: ./$EXE]\n\t-N [Path to F-Response INI. Default: ./$INI]\n"
 
-if [[ $EUID -ne 0 ]]; 
+if [ $# -gt 0 ]         #check for arguments
+then
+    while getopts "I:P:S:L:h" OPTION
+    do
+        case $OPTION in
+            P)
+            PORT=$OPTARG
+            ;;
+            S)
+            SERV=$OPTARG
+            ;;
+            I)
+            IPs=(`echo $OPTARG |sed 's/,/ /g'`)
+            ;;
+            L)
+            WIN=$OPTARG
+            ;;
+            E)
+            EXE=$OPTARG
+            ;;
+            N)
+            INI=$OPTARG
+            ;;
+            h)
+            echo -e $usage
+            exit 0
+            ;;
+            *)
+            # unknown option
+            echo "incorrect option $OPTION"
+            echo
+            echo -e $usage
+            echo
+            exit $bad
+            ;;
+        esac
+    done
+
+fi
+
+if [[ $EUID -ne 0 ]];
 then
    echo "This script must be run as root" 1>&2
-   exit -1
+   exit $bad
 fi
 
 
@@ -53,20 +98,49 @@ if [[ ! -f $EXE ]];
 then
    echo "File $EXE does not exist."
    echo "Please copy $EXE into this directory."
-   exit -1
+   exit $bad
 fi
 
 if [[ ! -f $INI ]];
 then
    echo "File $INI does not exist."
    echo "Please copy $INI into this directory."
-   exit -1
+   exit $bad
+fi
+
+if [[ "$REMOTEUSER" == "NONE" ]];
+then
+    echo -n "[?] Enter remote machine user name (to install agent) :> "
+    read REMOTEUSER
+fi
+
+if [[ "$REMOTEPASS" == "NONE" ]];
+then
+    echo -n "[?] Enter the remote machine password :> "
+    read -s REMOTEPASS
+    echo
+fi
+
+if [[ "$FRUSER" == "NONE" ]];
+then
+    echo -n "[?] Enter the F-Response user name :> "
+    read FRUSER
+fi
+
+if [[ "$FRPASS" == "NONE" ]];
+then
+    echo -n "[?] Enter the F-Response password :> "
+    read -s FRPASS
+    echo
 fi
 
 function installagent {
     IP=$1
     echo "[*] Installing the F-Response driver and starting its service on $IP"
     mkdir -p /mnt/forensics
+    if grep -qs "/mnt/forensics" /proc/mounts ; then
+        umount /mnt/forensics
+    fi
     mount -t cifs -o user="$REMOTEUSER%$REMOTEPASS",iocharset=utf8,file_mode=0777,dir_mode=0777 //$IP/c\$ /mnt/forensics
 
     if [[ $? -ne 0 ]];
@@ -97,7 +171,7 @@ function loginiscsi {
     echo
     echo "[*] Finding ISCSI targets for $IP"
 
-    iscsiadm --mode discoverydb --type sendtargets --portal  $IP --op new 
+    iscsiadm --mode discoverydb --type sendtargets --portal  $IP --op new
     iscsiadm --mode discoverydb  --type sendtargets --portal $IP --op update --name discovery.sendtargets.auth.username --value $FRUSER
     iscsiadm --mode discoverydb --type sendtargets --portal $IP --op update --name discovery.sendtargets.auth.password  --value $FRPASS
     MEM=`iscsiadm --mode discoverydb --type sendtargets --portal $IP  --discover|grep pmem$ |awk '{print $2}'|cut -d\: -f1`
@@ -129,14 +203,14 @@ function loginiscsi {
     echo MEM:
     readlink -f $MOUNTMEM
     echo
-    echo "[*] To use Volatility:"
+    echo "[*] To use Volatility (example):"
     echo "  $ sudo python vol.py -f `readlink -f $MOUNTMEM` --profile=PROFILE PLUGIN"
 
     echo
     echo RAW DISK:
     readlink -f $MOUNTDISK
     echo
-    echo "[*] To use sleuthkit: "
+    echo "[*] To use sleuthkit (example): "
     echo "  $ sudo mmls `readlink -f $MOUNTDISK`"
     echo "[*] For more, see http://wiki.sleuthkit.org/index.php?title=FS_Analysis"
 
@@ -144,7 +218,7 @@ function loginiscsi {
     echo VOLUME:
     readlink -f $MOUNTVOL
     echo
-    echo "[*] To mount the volume:"
+    echo "[*] To mount the volume (example):"
     echo "  $ sudo mount -t ntfs -o ro,show_sys_files,hide_hid_files `readlink -f $MOUNTVOL` /mnt/disk/"
     echo
     echo
@@ -152,9 +226,11 @@ function loginiscsi {
 }
 
 function waitfordisconnect {
-    echo "[*] Use another shell for analysis"
-    echo "    Note: you must be root in order to access the devices"
+    echo "[*] Please use another shell for analysis."
+    echo "    Note: you must be root in order to access the devices."
+    echo
     echo "[*] Press [ENTER] to disconnect....."
+    echo "    Note: this will close all devices."
     read any
 }
 
@@ -168,7 +244,7 @@ function logoutiscsi {
         IP1=$( echo $i |cut -d+ -f2 )
         if [ "$IP" == "$IP1" ]; then
             echo "[*] iscsiadm --mode node --targetname $item --portal $IP1 --logout"
-            iscsiadm --mode node --targetname $item --portal $IP1 --logout 
+            iscsiadm --mode node --targetname $item --portal $IP1 --logout
             RESOURCES=( "${RESOURCES[@]/$i}" )
         fi
     done
